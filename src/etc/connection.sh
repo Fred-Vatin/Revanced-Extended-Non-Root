484ee26a900966e148728531394b4fae23bc1a1b
Fred Vatin <22337329+Fred-Vatin@users.noreply.github.com>
2024-11-30 21:52:02 +0100

    Update connection.sh

--- src/etc/connection.sh
+++ src/etc/connection.sh
@@ -7,11 +7,11 @@ check_connection() {
 	wget -q $(curl -s "https://api.github.com/repos/revanced/revanced-patches/releases/tags/v4.17.0" | jq -r '.assets[] | select(.name == "patches.json") | .browser_download_url') || rm -f "patches.json"
 	if [ -f patches.json ]; then
 		echo "internet_error=0" >> $GITHUB_OUTPUT
-		echoOK "Github connection OK"
+		green_log "Github connection OK"
 		rm -f "patches.json"
 	else
 		echo "internet_error=1" >> $GITHUB_OUTPUT
-		echoErr "Github connection not stable!"
+		red_log "Github connection not stable!"
 	fi
 }
 check_connection
