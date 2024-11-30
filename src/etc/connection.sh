8f55a5373e79fc64187d984ba29c8da791fe7315
Fred Vatin <22337329+Fred-Vatin@users.noreply.github.com>
2024-11-30 21:22:07 +0100

    use utils.sh to echo in color

--- src/etc/connection.sh
+++ src/etc/connection.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+source ./utils.sh
+
 # Check github connection stable or not:
 check_connection() {
 	wget -q $(curl -s "https://api.github.com/repos/revanced/revanced-patches/releases/tags/v4.17.0" | jq -r '.assets[] | select(.name == "patches.json") | .browser_download_url') || rm -f "patches.json"
 	if [ -f patches.json ]; then
 		echo "internet_error=0" >> $GITHUB_OUTPUT
-		echo -e "\e[32mGithub connection OK\e[0m"
+		echoOK "Github connection OK"
 		rm -f "patches.json"
 	else
 		echo "internet_error=1" >> $GITHUB_OUTPUT
-		echo -e "\e[31mGithub connection not stable!\e[0m"
+		echoErr "Github connection not stable!"
 	fi
 }
 check_connection
