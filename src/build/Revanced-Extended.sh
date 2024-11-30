3b7daacae96bacae9a67d2a15d2b74505f012dce
Fred Vatin <22337329+Fred-Vatin@users.noreply.github.com>
2024-11-30 15:53:34 +0100

    keep youtube and music only

--- src/build/Revanced-Extended.sh
+++ src/build/Revanced-Extended.sh
@@ -22,14 +22,6 @@ revanced_dl(){
 	get_patches_key "youtube-revanced-extended"
 	split_editor "youtube" "youtube-armeabi-v7a" "exclude" "split_config.arm64_v8a split_config.x86 split_config.x86_64"
 	patch "youtube-armeabi-v7a" "revanced-extended" "inotia"
-	# Patch Youtube x86
-	get_patches_key "youtube-revanced-extended"
-	split_editor "youtube" "youtube-x86" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86_64"
-	patch "youtube-x86" "revanced-extended" "inotia"
-	# Patch Youtube x86_64
-	get_patches_key "youtube-revanced-extended"
-	split_editor "youtube" "youtube-x86_64" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86"
-	patch "youtube-x86_64" "revanced-extended" "inotia"
 }
 2() {
 	revanced_dl
@@ -43,26 +35,11 @@ revanced_dl(){
 	get_apk "com.google.android.apps.youtube.music" "youtube-music-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
 	patch "youtube-music-armeabi-v7a" "revanced-extended" "inotia"
 }
-3() {
-	revanced_dl
-	get_apk "com.google.android.youtube" "youtube-lite" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
-	# Patch YouTube Lite Arm64-v8a:
-	get_patches_key "youtube-revanced-extended"
-	split_editor "youtube-lite" "youtube-lite-arm64-v8a" "include" "split_config.arm64_v8a split_config.en split_config.xhdpi split_config.xxxhdpi"
-	patch "youtube-lite-arm64-v8a" "revanced-extended" "inotia"
-	# Patch YouTube Lite Armeabi-v7a:
-	get_patches_key "youtube-revanced-extended"
-	split_editor "youtube-lite" "youtube-lite-armeabi-v7a" "include" "split_config.armeabi_v7a split_config.en split_config.xhdpi split_config.xxxhdpi"
-	patch "youtube-lite-armeabi-v7a" "revanced-extended" "inotia"
-}
 case "$1" in
     1)
         1
         ;;
     2)
         2
-        ;;
-    3)
-        3
         ;;
 esac
