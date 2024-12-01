cbc9c09ebd345be313687c10e45aafe49f32cd69
Fred Vatin <22337329+Fred-Vatin@users.noreply.github.com>
2024-12-01 04:49:06 +0100

    Update setTag.sh

--- src/etc/setTag.sh
+++ src/etc/setTag.sh
@@ -13,17 +13,99 @@ else
   fi
 fi
 
-# Vérifier si un tag courant est passé en paramètre
-CURRENT_TAG=$1
+# display help function
+show_help() {
+    echo "Usage: $0 [--check <number>] [--set]"
+    echo "Use one or the other."
+    echo "--check"
+    echo "type: variable containing a number > 0."
+    echo "Returns a boolean and if false, abort workflow"
+    echo ""
+    echo "--set"
+    echo "type: boolean, default: true"
+    echo "Returns the next_tag in \$GITHUB_OUTPUT"
+}
 
-# Obtenir le dernier tag de GitHub
-TAG=$(gh release list -L 1 | awk -F '\t' '{print $3}')
-if [ -z "$TAG" ]; then TAG=$CURRENT_TAG; fi
+# Use getopt to process args
+OPTIONS=$(getopt -o hc: --long help,check,current: -- "$@")
+if [ $? -ne 0 ]; then
+    show_help
+    exit 1
+fi
 
-# Calculer le prochain tag
-NEXT_TAG=$((TAG + 1))
+eval set -- "$OPTIONS"
 
-# Afficher et enregistrer le prochain tag
-green_log "NEXT_TAG=$NEXT_TAG"
-echo "NEXT_TAG=$NEXT_TAG" >> $GITHUB_ENV
-echo "next_tag=$NEXT_TAG" >> $GITHUB_OUTPUT
+# init vars
+CHECK=""
+SET=false
+
+# read options
+while true; do
+    case "$1" in
+        -h|--help)
+            show_help
+            abort "setTag.sh needs one of these arguments above"
+            ;;
+        --check)
+            # if is empty or null
+            if [ -z "$2" ]; then
+                show_help
+                abort "--check is empty. It requires a positiver integer argument." 
+            fi
+            if is_string "$2"; then
+                show_help
+                abort "--check is a string with value: $2. It requires a non-negative integer argument."
+            fi
+            if is_positive_integer "$2"; then
+                CHECK=$2
+                green_log "Check next_tag OK with value: $2"
+                exit 0
+            else
+                show_help
+                abort "--check requires a non-negative integer argument."                
+            fi
+            ;;
+        --set)
+            SET=true
+            shift
+            ;;            
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Unknown option: $1"
+            show_help
+            abort "setTag.sh needs one of these arguments above"
+            ;;
+    esac
+done
+
+# if --check and --set are both used abort
+if [ "$CHECK" != "null" ] && [ "$SET" = true ]; then
+    show_help
+    abort "--check and --set options cannot be used together."
+fi
+
+# when --check is a positive number
+if [ "$CHECK" != "null" ]; then
+    echo "[--check] option is set with value: $CHECK"
+    exit 0    
+fi
+
+# when option --set is used
+if [ "$SET" = true ]; then
+    echo "[--set] option is used"
+
+    # Get last tag from the repo
+    TAG=$(gh release list -L 1 | awk -F '\t' '{print $3}')
+    if [ -z "$TAG" ]; then TAG=0; fi
+    
+    # Calculer le prochain tag
+    NEXT_TAG=$((TAG + 1))
+    
+    # Afficher et enregistrer le prochain tag
+    green_log "NEXT_TAG=$NEXT_TAG"
+    echo "NEXT_TAG=$NEXT_TAG" >> $GITHUB_ENV
+    echo "next_tag=$NEXT_TAG" >> $GITHUB_OUTPUT
+fi
