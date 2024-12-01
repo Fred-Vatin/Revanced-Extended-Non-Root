7d4883b3e3fe149e6718ecd765776115c1e257e8
Fred Vatin <22337329+Fred-Vatin@users.noreply.github.com>
2024-12-01 01:11:52 +0100

    Update setTag.sh

--- src/etc/setTag.sh
+++ src/etc/setTag.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 source ./src/etc/utils.sh
 
+# -- check if utils is loaded --
+if declare -f green_log > /dev/null; then
+  green_log "utils.sh has been loaded"
+else
+  message="utils.sh can not be loaded"
+  echo >&2 -e "\033[0;31m[-] $message\033[0m"
+  if [ "${GITHUB_REPOSITORY-}" ]; then 
+    echo -e "::error::$message\n"
+  fi
+fi
+
 # Vérifier si un tag courant est passé en paramètre
 CURRENT_TAG=$1
 
