2d17a13c698297591098e1a3f2ee0dfea06d7b14
Fred Vatin <22337329+Fred-Vatin@users.noreply.github.com>
2024-12-02 20:36:23 +0100

    Update utils.sh

--- src/etc/utils.sh
+++ src/etc/utils.sh
@@ -1,41 +1,55 @@
 #!/usr/bin/env bash
 
-green_log() { echo -e "\033[0;32m[+] ${1}\033[0m"; }
+green_log() {
+	if [ "${GITHUB_REPOSITORY-}" ]; then
+		echo -e "::notice::\033[0;32m${1}\033[0m\n"
+	else
+		echo -e "\033[0;32m[+] ${1}\033[0m"
+	fi
+}
 
 blue_log() { echo -e "\033[0;34m ${1}\033[0m"; }
 
-red_log() {
+warn_log() {
+	if [ "${GITHUB_REPOSITORY-}" ]; then
+		echo -e "::warning::\033[0;33m${1}\033[0m\n"
+	else
+		echo -e "\033[0;33m[!] ${1}\033[0m"
+	fi
+}
+
+error_log() {
 	echo >&2 -e "\033[0;31m[-] ${1}\033[0m"
-	if [ "${GITHUB_REPOSITORY-}" ]; then echo -e "::error::utils.sh [-] ${1}\n"; fi
+	if [ "${GITHUB_REPOSITORY-}" ]; then echo -e "::error::\033[0;31m${1}\033[0m\n"; fi
 }
 
 abort() {
-	red_log "ABORT: ${1-}"
+	error_log "ABORT: ${1-}"
 	exit 1
 }
 
 # check if value is positive number
 is_positive_integer() {
-    case $1 in
-        ''|*[!0-9]*)
-            # this is not a number
-            return 1
-            ;;
-        *)
-            # this is a positive number
-            return 0
-            ;;
-    esac
+	case $1 in
+	'' | *[!0-9]*)
+		# this is not a number
+		return 1
+		;;
+	*)
+		# this is a positive number
+		return 0
+		;;
+	esac
 }
 
 # check if $1 is a string
 is_string() {
-    case $1 in
-        *[![:alnum:]]*)
-            return 1
-            ;;
-        *)
-            return 0
-            ;;
-    esac
+	case $1 in
+	*[![:alnum:]]*)
+		return 1
+		;;
+	*)
+		return 0
+		;;
+	esac
 }
